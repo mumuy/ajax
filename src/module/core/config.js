355df4fce3dac12b91fc434d1ceadf3eb7ba4f53
mumuy <zhenghaole1990@126.com>
2025-07-11 01:55:39 +0800

    updapted 配置调整

--- src/module/core/config.js
+++ src/module/core/config.js
@@ -4,11 +4,12 @@ export default {
     url:location.origin,                        // 请求地址
     data:{},                                    // 请求参数
     dataType:'json',                            // 响应数据格式
-    headers: {},                                // 请求头设置
+    headers:{},                                 // 请求头设置
     crossDomain:true,                           // 是否跨域
     cache:false,                                // 是否设置缓存
     timeout:5000,                               // 超时时间
-    jsonp: 'callback',                          // jsonp回调函数引用
+    jsonp:'callback',                           // jsonp回调函数引用
     jsonpCallback: 'jsonp_' + Date.now(),       // jsonp回调函数名称
-    onTimeout:function(){},
+    onTimeout:function(){},                     // 超时处理
+    onUpdated:function(){},                     // 长链接更新数据
 }
